de7694f5f9c6c0c648184f87e3dcd7ec40eaf648
Nikhil Anand <anand.nikhil@gmail.com>
2023-02-17 17:06:43 -0800

    Fix

--- client/src/Panels/Main.tsx
+++ client/src/Panels/Main.tsx
@@ -2,9 +2,11 @@ import Links from "../Components/Links";
 import Search from "../Components/Search";
 import ListeningTime from "../Components/ListeningTime";
 
+import styles from "./Main.module.scss"
+
 const Component = () => {
   return (
-    <div className="panel">
+    <div className={styles.main}>
       <Search />
       <Links />
       <ListeningTime />
