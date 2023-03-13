39e9ae414eb4595bd70e505879b3ccb475d5ebdd
Nikhil Anand <anand.nikhil@gmail.com>
2023-03-13 07:45:51 -0700

    bulb

--- client/src/Components/DarkMode.tsx
+++ client/src/Components/DarkMode.tsx
@@ -1,4 +1,5 @@
 import { FiSun, FiMoon } from "react-icons/fi";
+import { TbBulbOff, TbBulb } from "react-icons/tb";
 import { useStore } from "../State";
 
 import styles from "./DarkMode.module.scss";
@@ -13,11 +14,11 @@ export default () => {
     <div className={styles.component}>
       {darkMode ? (
         <button onClick={() => set.preferences.darkMode(false)}>
-          <FiSun /> <span>Light Mode</span>
+          <TbBulb /> <span>Light Mode</span>
         </button>
       ) : (
         <button onClick={() => set.preferences.darkMode(true)}>
-          <FiMoon /> <span>Dark Mode</span>
+          <TbBulbOff /> <span>Dark Mode</span>
         </button>
       )}
     </div>
